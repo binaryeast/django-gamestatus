9aedc569a1ea5e261efb7077fad63092c3d95f77
binaryeast <binaryeast@gmail.com>
2021-11-22 08:57:42 +0000

    update settings

--- stat_window/settings.py
+++ stat_window/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cs2of7kp9zl26d=akg1=pfjq06n*521154+@o7_0=b(gcq3w+n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'status',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
